d9d71d6e3947c131b7d27b461d8e32b8d513ff12
Alon (Ubuntu) <downtown2u@gmail.com>
2022-02-05 18:03:12 +0200

    🎨 Pie labels should now stay in frame

--- scripts/update_readme.py
+++ scripts/update_readme.py
@@ -17,10 +17,10 @@ SEARCH_FILE = '*.cpp'  # TODO: change to cptk recipe
 
 COLORS = [
     '#0094C6',
-    "#13C4A3",
-    "#4BC0C0",
-    "#005E7C",
-    "#84DD63",
+    '#13C4A3',
+    '#4BC0C0',
+    '#005E7C',
+    '#84DD63',
     '#36F1CD',
     '#B0FE76'
 ]
@@ -74,27 +74,48 @@ def load_folders_data(root: str) -> list[Folder]:
 
 
 def folders_pie_data(data: list[Folder], colors: list[str]) -> dict:
+    LABEL_COLOR = 'white'
+    TITLE_COLOR = '#aaa'
+
     return {
-        "type": "outlabeledPie",
-        "data": {
-            "labels": [d.name for d in data],
-            "datasets": [{
-                "backgroundColor": colors,
-                "data": [len(d.recipes) for d in data],
+        'type': 'outlabeledDoughnut',
+        'data': {
+            'labels': [d.name for d in data],
+            'datasets': [{
+                'backgroundColor': colors,
+                'data': [len(d.recipes) for d in data],
             }]
         },
-        "options": {
-            "plugins": {
-                "legend": False,
-                "outlabels": {
-                    "text": "%l (%v)",
-                    "color": "white",
-                    "stretch": 52,
-                    "font": {
-                        "resizable": True,
-                        "minSize": 24,
-                        "maxSize": 32,
+        'options': {
+            'plugins': {
+                'legend': False,
+                'outlabels': {
+                    'text': '%l (%v)',
+                    'color': LABEL_COLOR,
+                    'stretch': 35,
+                    'borderWidth': 2,
+                    'borderRadius': 10,
+                    'textAlign': 'center',
+                    'font': {
+                        'style': 'bold',
+                        'resizable': True,
+                        'minSize': 8,
+                        'maxSize': 20,
                     }
+                },
+                'doughnutlabel': {
+                    'labels': [
+                        {
+                            'text': sum(len(d.recipes) for d in data),
+                            'color': TITLE_COLOR,
+                            'font': {'size': 20, 'weight': 'bold'},
+                        },
+                        {
+                            'text': 'total',
+                            'font': {'size': 12},
+                            'color': TITLE_COLOR,
+                        }
+                    ]
                 }
             }
         }
@@ -103,13 +124,16 @@ def folders_pie_data(data: list[Folder], colors: list[str]) -> dict:
 
 def create_jinja_env():
     folders_data = load_folders_data(ROOT)
-    chart_data = folders_pie_data(folders_data, COLORS[:len(folders_data)])
+    chart_data = folders_pie_data(
+        folders_data[::-1],
+        COLORS[:len(folders_data)]
+    )
 
     env = jinja2.Environment()
 
     env.globals.update({
         'foldersData': folders_data,
-        'foldersPieUrl': chart_url(chart_data, width=800, height=800),
+        'foldersPieUrl': chart_url(chart_data, width=450, height=450),
         'problemsCount': sum(len(folder.recipes) for folder in folders_data),
     })
 
