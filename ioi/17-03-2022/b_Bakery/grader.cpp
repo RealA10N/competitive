a21385ce81237b5119360bf25057aad25fda54c1
Alon (Ubuntu) <downtown2u@gmail.com>
2022-03-18 01:12:30 +0200

    ☑️ b_Bakery - IOI train 17-03-2022

--- ioi/17-03-2022/b_Bakery/grader.cpp
+++ ioi/17-03-2022/b_Bakery/grader.cpp
@@ -1,15 +1,10 @@
-#include "bakery.h"
-#include <iostream>
-#include <cstdio>
-#include <cstdlib>
-#include <vector>
-
+#include <bits/stdc++.h>
 using namespace std;
+#include "bakery.h"
+
 typedef long long ll;
 
-void Inv(ll num) {
-    cout << num << endl;
-}
+void Inv(ll num) { cout << num << endl; }
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -18,9 +13,7 @@ int main() {
     int N;
     cin >> N;
     vector<ll> A(N);
-    for (int i = 0; i < N; ++i) {
-        cin >> A[i];
-      }
-    ProblemLaPastry (N, A);
+    for (int i = 0; i < N; ++i) cin >> A[i];
+    ProblemLaPastry(N, A);
     return 0;
 }
