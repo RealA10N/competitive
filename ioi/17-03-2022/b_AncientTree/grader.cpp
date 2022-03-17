36334bcea96d15bcbdaf8cb92184185fb5e1d222
Alon (Ubuntu) <downtown2u@gmail.com>
2022-03-18 01:01:29 +0200

    ☑️ b_AncientTree - IOI train 17-03-2022

--- ioi/17-03-2022/b_AncientTree/grader.cpp
+++ ioi/17-03-2022/b_AncientTree/grader.cpp
@@ -1,35 +1,20 @@
+#include <bits/stdc++.h>
+
 #include "AncientTree.h"
-#include <iostream>
-#include <cstdio>
-#include <cstdlib>
-#include <string>
-#include <vector>
 using namespace std;
 
-
-void UList(std::vector<int> &U){
-	if(U[0]==-1){
-		cout<<"Error\n";
-	}
-	else{
-		for(int num:U){
-			cout <<num<<"\n";
-		}
-	}
+void UList(std::vector<int> &U) {
+    for (int num : U) cout << num << "\n";
 }
 
-
-int main(){
-	ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-	int N;
-	cin >> N;
-
-	vector<int> V(N);
-
-    for (int i = 0; i < N; i++) {
-        cin >> V[i]; 
-    }
-	
-	RestoreAncientTree(N, V);
-	return 0;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int N;
+    cin >> N;
+    vector<int> V(N);
+    for (int i = 0; i < N; i++) cin >> V[i];
+    RestoreAncientTree(N, V);
+    return 0;
 }
