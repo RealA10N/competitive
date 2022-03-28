cf826b5828be4d9b9b0d4a038a0fdc52e59b2498
Alon (Ubuntu) <downtown2u@gmail.com>
2022-03-28 19:30:32 +0300

    ☑️ b_TheBigTrip - IOI train 24-03-2022

--- ioi/24-03-2022/b_TheBigTrip/grader.cpp
+++ ioi/24-03-2022/b_TheBigTrip/grader.cpp
@@ -1,33 +1,30 @@
-#include "TheBigTrip.h"
-#include <iostream>
 #include <cstdio>
 #include <cstdlib>
+#include <iostream>
 #include <vector>
 
-int readInt(){
+#include "TheBigTrip.h"
+
+int readInt() {
     int i;
-    if(scanf("%d",&i)!=1){
-        fprintf(stderr,"Error while reading input\n");
+    if (scanf("%d", &i) != 1) {
+        fprintf(stderr, "Error while reading input\n");
         exit(1);
     }
     return i;
 }
 
-int main(){
+int main() {
     std::ios_base::sync_with_stdio(0);
     int N, W;
     N = readInt();
     W = readInt();
     std::vector<int> H(N);
-    for(int i=1; i<=N; i++){
-        H[i-1] = readInt();
+    for (int i = 1; i <= N; i++) {
+        H[i - 1] = readInt();
     }
-        
+
     std::vector<long long> ans = GetWaitTimes(N, W, H);
-    printf("%lld %lld", ans[0],ans[1]);
-
+    printf("%lld %lld\n", ans[0], ans[1]);
     return 0;
-
-
-
 }
