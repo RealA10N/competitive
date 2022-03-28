7a023cacb5cd3774a3bc91f90053739ec7cb7042
Alon (Ubuntu) <downtown2u@gmail.com>
2022-03-28 13:15:08 +0300

    ☑️ b_StackOfCubes - IOI train 10-03-2022

--- ioi/10-03-2022/b_StackOfCubes/StackOfCubes.cpp
+++
@@ -1,7 +0,0 @@
-#include <bits/stdc++.h>
-#include "StackOfCubes.h"
-using namespace std;
-
-string FinalStack(int N, std::vector<string> stacks, std::vector<int> A, std::vector<int> B){
-	//TODO: implement your solution here
-}
