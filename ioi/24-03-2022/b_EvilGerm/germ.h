9a6c40599947973d8c68c069dfda7c822cc59c4f
Alon (Ubuntu) <downtown2u@gmail.com>
2022-03-28 21:14:38 +0300

    ☑️ b_EvilGerm - IOI train 24-03-2022

--- ioi/24-03-2022/b_EvilGerm/germ.h
+++
@@ -1,3 +0,0 @@
-#include <vector>
-using namespace std;
-int EvilGerm (int R,int N,int A,int B,int X,vector<int> S);
--- ioi/24-03-2022/b_EvilGerm/grader.cpp
+++ ioi/24-03-2022/b_EvilGerm/grader.cpp
@@ -1,23 +1,23 @@
 #include <bits/stdc++.h>
-#include "germ.h"
-#include <iostream>
+
 #include <cstdio>
 #include <cstdlib>
+#include <iostream>
 #include <string>
 #include <vector>
+
+#include "EvilGerm.h"
 using namespace std;
 
-
-
-
 int main() {
-    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-    int R,N,A,B,X;  
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int R, N, A, B, X;
     cin >> R >> N >> A >> B >> X;
-    vector<int> S(X, 0);    
+    vector<int> S(X, 0);
     for (int i = 0; i < X; ++i) cin >> S[i];
-    int ans= EvilGerm (R,N,A,B,X,S);
+    int ans = EvilGerm(R, N, A, B, X, S);
     cout << ans << endl;
-
     return 0;
 }
