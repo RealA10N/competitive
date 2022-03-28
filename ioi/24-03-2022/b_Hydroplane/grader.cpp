9448c7b65d9d88824ff87e2aa1ea831967b543d2
Alon (Ubuntu) <downtown2u@gmail.com>
2022-03-28 20:23:07 +0300

    ☑️ b_Hydroplane - IOI train 24-03-2022

--- ioi/24-03-2022/b_Hydroplane/grader.cpp
+++ ioi/24-03-2022/b_Hydroplane/grader.cpp
@@ -1,33 +1,29 @@
-#include "Hydroplane.h"
-#include <iostream>
 #include <cstdio>
 #include <cstdlib>
+#include <iostream>
 #include <string>
 #include <vector>
+
+#include "Hydroplane.h"
 using namespace std;
 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
 
-int main(){
-	ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-	int N, M, H, S, T;
-	cin >> N >> M >> H >> S >> T;;
+    int N, M, H, S, T;
+    cin >> N >> M >> H >> S >> T;
 
-	vector<int> A(M);
-	vector<int> B(M);
-	vector<int> C(M);
-
+    vector<int> A(M), B(M), C(M);
     for (int i = 0; i < M; i++) {
-        cin >> A[i]>>B[i]>>C[i];
+        cin >> A[i] >> B[i] >> C[i];
     }
-	
-	vector<int> X(H);
-	vector<int> Y(H);
 
+    vector<int> X(H), Y(H);
     for (int i = 0; i < H; i++) {
-        cin >> X[i]>>Y[i];
+        cin >> X[i] >> Y[i];
     }
-	
-	cout<<MinimalCost(N, M, H, S, T, A, B, C, X, Y)<<"\n";
-	
-	return 0;
+
+    cout << MinimalCost(N, M, H, S, T, A, B, C, X, Y) << "\n";
+    return 0;
 }
